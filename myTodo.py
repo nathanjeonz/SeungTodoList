63ef2d91c867d3493d7fdf1c6dbbd4797f80aaf1
nathanjeonz <jeonz0102_gmail.com>
2025-08-01 19:15:38 +0700

    going to publish it online

--- myTodo.py
+++ myTodo.py
@@ -5,13 +5,25 @@ import flask
 
 app = flask.Flask(__name__)
 
-tasks = ['i have to go to school','i have to sleep 10pm sharp','i have to complete my homew']
+tasks = ['i have to go to school','i have to sleep 10pm sharp','i have to complete my homework']
 
 @app.route('/')
 def home():
     return flask.render_template('index.html',seung_task=tasks)
 
+@app.route('/submit',methods=['POST'])
+def submitf():
+    todo = flask.request.form['entertodo']
+    tasks.append(todo)
+    return flask.redirect('/')
 
+@app.route('/delete/<int:index>',methods=['POST'])
+def remove(index):
+    tasks.pop(index)
+    return flask.redirect('/')
+
+#     tasks.remove(ToDo)
+#     return f"you deleted task {ToDo}"
 
 @app.route('/seung')
 def hello():
