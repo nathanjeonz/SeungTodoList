dd05ab1b253eb33e97677c188c7c621559d3c583
nathanjeonz <jeonz0102_gmail.com>
2025-08-17 20:53:05 +0700

    finished

--- myTodo.py
+++ myTodo.py
@@ -1,25 +1,42 @@
 import flask
+import json
 
+def load_tasks():
+    try:
+        with open("todos.json") as file:
+            res = json.load(file)
+            return res['todos']
+    except FileNotFoundError:
+        return []
 
-
+def save_tasks(tasks):
+    with open("todos.json", "w") as file:
+        json.dump({"todos": tasks}, file)
 
 app = flask.Flask(__name__)
 
-tasks = ['i have to go to school','i have to sleep 10pm sharp','i have to complete my homework']
+
 
 @app.route('/')
 def home():
+    tasks = load_tasks()
     return flask.render_template('index.html',seung_task=tasks)
 
 @app.route('/submit',methods=['POST'])
 def submitf():
     todo = flask.request.form['entertodo']
+    tasks = load_tasks()
     tasks.append(todo)
+    save_tasks(tasks)
+    
     return flask.redirect('/')
 
 @app.route('/delete/<int:index>',methods=['POST'])
 def remove(index):
+    tasks = load_tasks()
     tasks.pop(index)
+    save_tasks(tasks)
+
     return flask.redirect('/')
 
 
